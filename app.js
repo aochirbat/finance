56efbbad6279be2aaee58ab4fb465ecf14561bc0
ochirbat <ochkiwork@gmail.com>
2020-06-20 19:18:52 +0800

    Finance data uusgelee

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function(){
- 
+    var DOMstrings ={
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value', 
+        addBtn: '.add__btn',
+    };
+    return { 
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, 
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDomStrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 
 
 //Санхүүтэй ажиллах контроллер
 var financeController = (function(){
-    
+        var data = {
+            allItems:{
+                inc : [],
+                exp : []
+            },
+            totals: {
+                inc: 0,
+                exp: 0
+            }
+        }
+        var Income = function(id , description , value){
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+        var Expense = function(id , description , value){
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
 })();
 
 //Програмын холбогч контроллер
 var appController = (function(uiController , fnController){
-
-    var ctrlAddItem = function(){
+        var ctrlAddItem = function(){
           // 1. Оруулах өгөгдөлийг дэлгэцээс олж авна.
-            console.log("bolson");
+            console.log(uiController.getInput());
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
 
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана 
@@ -23,19 +58,26 @@ var appController = (function(uiController , fnController){
 
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     }
-
-    document.querySelector(".add__btn").addEventListener("click" , function(){
-      ctrlAddItem();
-    });
-    document.addEventListener("keypress", function(event){
-        if(event.keyCode === 13 || event.which === 13) {
+    var setupEventListeners = function(){
+        var DOM = uiController.getDomStrings();
+        document.querySelector(DOM.addBtn).addEventListener("click" , function(){
             ctrlAddItem();
-        } else {
-            console.log('oor tovch darj bna');
+          });
+          document.addEventListener("keypress", function(event){
+              if(event.keyCode === 13 || event.which === 13) {
+                  ctrlAddItem();
+              } else {
+                  // console.log('oor tovch darj bna');
+              }
+          });
+    };
+    return {
+        init: function (){
+            console.log('Application started.....');
+            setupEventListeners();
         }
-        
-    });
-
-
-
+    }
+    
 })(uiController , financeController);
+
+appController.init();
